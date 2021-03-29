a82b35e5c91b6080acb0d0423e284ad35a80e07d
Awais Gill <awaisgill245@gmail.com>
2021-03-30 03:29:29 +0500

    fix scroll to top button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// gotp to top
+const backToTopButton = document.querySelector("#back-to-top");
+window.addEventListener("scroll", scrollFunction);
+function scrollFunction() {
+  if (window.pageYOffset > 200) {
+    backToTopButton.style.display = "block"; //show backToTopButton
+  }
+  else {
+    backToTopButton.style.display = "none"; //hide backToTopButton
+  }
+}
+
+
 // replace gallery image of product
 var productImg = document.getElementById('productImage');
 var smallImg = document.getElementsByClassName('small-image');
@@ -10,20 +23,3 @@ smallImg[1].onclick = function()
 {
   productImg.src = smallImg[1].src;
 }
-
-// gotp to top
-// var topbtn = document.getElementById("gotopbtn");
-// window.onscroll = function() {
-//   showBtn()
-// };
-
-// function showBtn(){
-//   if(document.body.scrollTop > 800 || document.documentElement.scrollTop > 800)
-//   {
-//     topbtn.style.display = "block";
-//   }
-//   else
-//   {
-//     topbtn.style.display = "none";
-//   }
-// }
